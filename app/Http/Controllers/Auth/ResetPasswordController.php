b35d11a035304bfa412acc4a869767a0d7aa7936
Tavo Nieves J <ganieves@outlook.com>
2021-02-08 19:53:25 -0500

    Indoctrinate ResetPasswordController.php

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -4,9 +4,12 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers\Auth;
 
+use App\Entity\User;
 use App\Http\Controllers\AbstractController;
 use App\Providers\RouteServiceProvider;
+use App\Repository\UserRepositoryInterface;
 use Illuminate\Foundation\Auth\ResetsPasswords;
+use Illuminate\Support\Str;
 
 final class ResetPasswordController extends AbstractController
 {
@@ -14,4 +17,15 @@ final class ResetPasswordController extends AbstractController
 
     /** @var string */
     protected $redirectTo = RouteServiceProvider::HOME;
+
+    protected function resetPassword(User $user, string $password): void
+    {
+        $user->setPassword($password);
+        $user->setRememberToken(Str::random(10));
+
+        $userRepository = app(UserRepositoryInterface::class);
+        $userRepository->save($user);
+
+        $this->guard()->login($user);
+    }
 }
